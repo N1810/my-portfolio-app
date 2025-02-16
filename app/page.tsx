d8503a83cc8bb6462df9b82e422a34ca9f7ce7d2
Neeraj Kumar <neeraj.kumar@eleva8e.com>
2025-02-16 19:24:13 +0530

    Project Setup

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+    return (
+        <>
+            <div className="relative bg-black">
+                <h1>Hello! Portfolio</h1>
+            </div>
+        </>
+    );
+}
